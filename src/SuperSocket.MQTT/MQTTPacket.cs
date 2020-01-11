10f021ae794f313f4bdd3578ea29a493a8f02337
Kerry Jiang <kerry-jiang@hotmail.com>
2020-01-11 00:44:57 -0800

    let MQTTPacket implement IKeyedPackageInfo

--- src/SuperSocket.MQTT/MQTTPacket.cs
+++ src/SuperSocket.MQTT/MQTTPacket.cs
@@ -4,12 +4,14 @@ using SuperSocket.ProtoBase;
 
 namespace SuperSocket.MQTT
 {
-    public abstract class MQTTPacket
+    public abstract class MQTTPacket : IKeyedPackageInfo<ControlPacketType>
     {
         public ControlPacketType Type { get; set; }
 
         public byte Flags { get; set; }
-        
+
+        ControlPacketType IKeyedPackageInfo<ControlPacketType>.Key => Type;
+
         internal protected abstract void DecodeBody(ref SequenceReader<byte> reader, object context);
 
         public abstract int EncodeBody(PipeWriter writer);
