e544dbf58716a78ba6da5b73a7e8c5592655f2ab
shuang888 <luxiaoshuang@foxmail.com>
2020-12-14 23:17:21 +0800

    compatible v5.0 and QOS1 QOS2

--- src/SuperSocket.MQTT/Packets/PublishPacket.cs
+++ src/SuperSocket.MQTT/Packets/PublishPacket.cs
@@ -21,6 +21,14 @@ namespace SuperSocket.MQTT.Packets
             Qos = (this.Flags - 0x30) / 2;
             reader.TryReadBigEndian(out short protocolNameLen);
             TopicName = reader.ReadString(protocolNameLen, Encoding.UTF8);
+            if (Qos > 0)
+            {
+                reader.TryRead(out byte msb);
+                reader.TryRead(out byte lsb1);
+                reader.TryPeek(out byte lsb2);
+                if (lsb2 == 0)
+                    reader.Advance(1);
+            }
             TopicBody = reader.ReadString((int)reader.Remaining, Encoding.UTF8);
             TopicData = reader.Sequence.ToArray();
         }
